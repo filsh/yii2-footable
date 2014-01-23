33ce207f262d5c73c2c5030024fe1b03a74c4b65
Maliy Igor <imaliy.filsh@gmail.com>
2014-01-23 08:43:07 +0200

    Footable GridView

--- GridView.php
+++ GridView.php
@@ -4,9 +4,18 @@ namespace yii\footable;
 
 class GridView extends \yii\grid\GridView
 {
+    public $footableOptions = [];
+    
     public function run()
     {
         FootableAsset::register($this->getView());
+        $this->registerScript();
         parent::run();
+    }
+    
+    protected function registerScript()
+    {
+        $configure = !empty($this->footableOptions) ? Json::encode($this->footableOptions) : '';
+        $this->getView()->registerJs("jQuery('#{$this->options['id']} table').footable({$configure});");
     }
 }
